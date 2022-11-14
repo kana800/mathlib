89e35a4aa43115b0d49e9c062cea39da771c37d6
Chathura Nimesh <64830745+kana800@users.noreply.github.com>
2022-11-14 22:17:48 +0530

    changed name from test to tests

---
+++ tests/LinearGrapherTest.c
@@ -0,0 +1,17 @@
+#include "../lineargrapher/inputstring.h"
+
+#include "Windows.h"
+
+#include <check.h>
+
+START_TEST (test_name)
+{
+  /* unit test code */
+}
+END_TEST
+
+
+int main(void)
+{
+    return 0;
+}
